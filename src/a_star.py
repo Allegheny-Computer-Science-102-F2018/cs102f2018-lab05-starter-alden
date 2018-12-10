4c36c69fd4287c5392f0750822984a01255cc2b3
liux2 <liux2@allegheny.edu>
2018-12-10 18:56:16 -0500

    with errors

--- src/a_star.py
+++ src/a_star.py
@@ -1,6 +1,3 @@
-import argparse
-import implementation
-
 def get_matrix(content):
 # get the info of map, length and width
 # and the location of obstacles
@@ -22,23 +19,64 @@ def get_matrix(content):
     # get exit point, -1 because starts from 0
     exitP = (length-1, width-1)
     return length, width, enterP, exitP, obstacles
+# length 0, width 1, enterP 2, exitP 3, obstacles 4
 
+# https://www.redblobgames.com/pathfinding/a-star/
 def heuristic(a, b):
     # 4-way Manhatten distance
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
 
-def a_star():
+def a_star(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
     # Open and closed list
     came_from = {}
     cost_so_far = {}
     came_from[start] = None
-    Cost_so_far[start] = 0
+    cost_so_far[start] = 0
 
+    while not frontier.empty():
+        current = frontier.get()
 
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+def draw_tile(graph, id, style, width):
+    r = "."
+    if 'point_to' in style and style['point_to'].get(id, None) is not None:
+        (x1, y1) = id
+        (x2, y2) = style['point_to'][id]
+        if x2 == x1 + 1: r = ">"
+        if x2 == x1 - 1: r = "<"
+        if y2 == y1 + 1: r = "v"
+        if y2 == y1 - 1: r = "^"
+    if 'start' in style and id == style['start']: r = "A"
+    if 'goal' in style and id == style['goal']: r = "Z"
+    if 'path' in style and id in style['path']: r = "@"
+    if id in graph.walls: r = "#" * width
+    return r
+
+def draw_grid(command, graph, width=2, **style):
+    with open(command, 'w') as f:
+        for y in range(graph.height):
+            for x in range(graph.width):
+                f.write(b"%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
 
 if __name__ == "__main__":
+    import argparse
+    from implementation import *
     # argumant to get the map file path from terminal commend
     parser = argparse.ArgumentParser(description='Input your designed map file path.')
     parser.add_argument('-m', '--map', required=True, help = 'path to the map file')
@@ -54,6 +92,12 @@ if __name__ == "__main__":
             content = content + l
         print(content)
         data_tuple = get_matrix(content)
-        print(data_tuple)
-    # with open(args.solution, 'w') as f:
-    #     print("\n\nHere is the solution: \n\n")
+        #define entry and exit then pass to a_star
+        start, goal = data_tuple[2], data_tuple[3]
+        diagram = GridWithWeights(data_tuple[0], data_tuple[1])
+        diagram.walls = data_tuple[4]
+        #diagram.weights =
+        came_from, cost_so_far = a_star(diagram, start, goal)
+        # write answers
+        draw_grid(args.solution, diagram, width=3, point_to=came_from, start=start, goal=goal)
+        print("\n\nHere is the solution: \n\n")
--- src/implementation.py
+++ src/implementation.py
@@ -8,22 +8,44 @@
 import collections
 import heapq
 
-class SimpleGraph:
+class PriorityQueue:
     def __init__(self):
-        self.edges = {}
-
-    def neighbors(self, id):
-        return self.edges[id]
-
-class Queue:
-    def __init__(self):
-        self.elements = collections.deque()
+        self.elements = []
 
     def empty(self):
         return len(self.elements) == 0
 
-    def put(self, x):
-        self.elements.append(x)
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
 
     def get(self):
-        return self.elements.popleft()
+        return heapq.heappop(self.elements)[1]
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, id):
+        return id not in self.walls
+
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x + y) % 2 == 0: results.reverse() # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
